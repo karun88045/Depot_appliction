ad70917962cfb9d66eb47007467698fae5730bb9
karun88045 <arunkumar8804590163@gmail.com>
2023-06-03 10:47:51 +0530

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < ActionController::Base
   before_action :authorize
+  before_action :set_i18n_locale_from_params
+
+  protected
+   def set_i18n_locale_from_params
+     if params[:locale]
+       if I18n.available_locales.map(&:to_s).include?(params[:locale])
+         I18n.locale = params[:locale]
+       else
+         flash.now[:notice] = "#{params[:locale]} translation not available"
+         logger.error flash.now[:notice]
+       end
+     end
+   end
+   
+  def index
+    @greeting = t('hello')
+  end
+
+  def set_locale
+
+  end
+
   protected
   
     def authorize
